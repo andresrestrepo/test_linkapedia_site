edf333c6cea6c6e32c12f87fa5d0f0a55652b9b3
alexanderagudelo@linkapedia.com <alexanderagudelo@linkapedia.com>
2015-05-07 12:34:48 -0500

    load more functionality

--- public/javascripts/blogs/landing.js
+++ public/javascripts/blogs/landing.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
     $('.accordion-container').on('click', '.box p', onClickNextBox);
+    $('.accordion-container').on('click', '.box [url-more] span.glyphicon-chevron-down', onClickMore);
 });
 
 function onClickNextBox() {
@@ -15,6 +16,30 @@ function onClickNextBox() {
     }
 }
 
+function onClickMore() {
+    var that = $(this);
+    that.removeClass('glyphicon-chevron-down').addClass('glyphicon-refresh refresh-animate');
+
+    $.ajax({
+        url:that.parent().attr('url-more'),
+        dataType:'json'
+    }).done(function(res){
+        var items = getItemsFromResponse(res);
+        var box = that.parent().siblings('div');
+
+        $.each(items, function(i, item){
+            box.append($('<p>').text(item.name).attr('url-next', getHrefFromItem(item)));
+        });
+
+        if(res._links.next){
+            that.addClass('glyphicon-chevron-down').removeClass('glyphicon-refresh refresh-animate')
+            that.parent().attr('url-more', res._links.next.href);
+        }else{
+            that.remove();
+        }
+    }).fail(function(err){});
+}
+
 function getNextBox(urlNext, level) {
     $.ajax({
         url: urlNext,
@@ -24,27 +49,37 @@ function getNextBox(urlNext, level) {
         var json = parseJsonHalToBox(res);
         var container = $('<div>').addClass('box').attr('level', ++level), box = $('<div>');
 
-        if (json.urlmore) {
-            container.append($('<div>').text('more').attr('url-more', json.urlmore));
-        }
-
         $.each(json.items, function (i, item) {
             box.append($('<p>').text(item.name).attr('url-next', item.urlnext));
         });
 
-        $('.accordion-container').append(container.append(box));
-    }).fail(function (err) {});
+        container.append(box);
+
+        if (json.urlmore) {
+            container.append($('<div><span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></div>').attr('url-more', json.urlmore));
+        }
+
+        $('.accordion-container').append(container);
+    }).fail(function (err) {
+    });
 }
 
 function parseJsonHalToBox(res) {
     var box = {}, href;
     box.urlmore = res._links.next ? res._links.next.href : undefined;
-    for(var key in res._embedded)if(/taxonomies|topics/.test(key))box.items = res._embedded[key];
+    box.items = getItemsFromResponse(res);
 
-    $.each(box.items, function(i){
-        for(var key in box.items[i]._links)if(/discussions|topics|documents/.test(key))href = box.items[i]._links[key].href;
-        box.items[i].urlnext = href;
+    $.each(box.items, function (i) {
+        box.items[i].urlnext = getHrefFromItem(box.items[i]);
     });
 
     return box;
+}
+
+function getItemsFromResponse(res){
+    for (var key in res._embedded)if (/domains|taxonomies|topics/.test(key))return res._embedded[key];
+}
+
+function getHrefFromItem(item){
+    for (var key in item._links)if (/discussions|topics|documents/.test(key))return item._links[key].href;
 }
